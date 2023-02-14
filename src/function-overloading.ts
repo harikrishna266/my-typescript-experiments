99f2a42cfd1515ee52dcd1ce60741de8d40eb770
hari <harikrishna266@gmail.com>
2023-02-13 21:27:45 -0500

    simple function

--- src/function-overloading.ts
+++ src/function-overloading.ts
@@ -10,23 +10,27 @@ function parseCoOrdinateFromObject(cord: coOrdinate): coOrdinate {
     }
 }
 
-function parseCoOrdinateFromNumbers(lat: coOrdinate["lat"], long: coOrdinate['long']): coOrdinate {
-     return {
-        lat, long
-    }
-}
 
-
+function parseCoOrdinates(a: string): coOrdinate;
 function parseCoOrdinates(a: coOrdinate): coOrdinate;
 function parseCoOrdinates(a: coOrdinate['lat'], b: coOrdinate['long']): coOrdinate;
 function parseCoOrdinates(a: unknown, b?: unknown) {
-    if(typeof a === "object") {
-        return  parseCoOrdinateFromObject(a as coOrdinate);
+    if(typeof a === "object" ) {
+        return {
+            long: (a as coOrdinate).long,
+            lat: (a as coOrdinate).lat
+        }
+    }
+    else if(typeof a === 'string') {
+        // a.split(',').map((e) => )
     }
     else {
-        return parseCoOrdinateFromNumbers(a as number, b as number);
+        return {
+            a, b
+        }
     }
 }
 
 parseCoOrdinates({lat:1, long: 1});
 parseCoOrdinates(1, 1);
+parseCoOrdinates('lat:1,long: 1');
